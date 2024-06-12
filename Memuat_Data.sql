570634ee7029f67d04f76238d2235622100dd58b
hendriansyah-aja <74650226+hendriansyah-aja@users.noreply.github.com>
2024-06-13 05:30:40 +0700

    Create Memuat_Data.sql

---
+++ Memuat_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO DataWarehouse (EmployeeId, FullName, BirthDate, Address, PosId, PosTitle, StartDate, EndDate)
+VALUES (10105001, 'Ali Anton', '19-Aug-82', 'Jakarta Utara', '50001', 'IT Sr. Manager', '1-Mar-2022', '31-Dec-2022'),
+       (10105002, 'Rara Siva', '1-Jan-82', 'Mandalika', '50003', 'Sr. Programmer Analyst', '1-Mar-2022', '31-Dec-2022'),
+       (10105003, 'Rini Aini', '20-Feb-82', 'Sumbawa Besar', '50005', 'IT Secretary', '1-Mar-2022', '31-Dec-2022');
