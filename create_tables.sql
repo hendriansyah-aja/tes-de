96e4480ae4d5244860c1843963656042001e82e0
hendriansyah-aja <74650226+hendriansyah-aja@users.noreply.github.com>
2024-06-12 20:51:05 +0700

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -5,3 +5,13 @@ CREATE TABLE Employee (
     BirthDate DATE NOT NULL,
     Address VARCHAR(500)
 );
+
+CREATE TABLE PositionHistory (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    PosId VARCHAR(10) NOT NULL,
+    PosTitle VARCHAR(100) NOT NULL,
+    EmployeeId VARCHAR(10) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE,
+    FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId)
+);
